5c481627f50592a22fe217f1fb2213b374a732b7
baronchibuikem <baronchibuike@gmail.com>
2020-04-24 09:29:16 +0100

    added hosted url for the backend api

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -4,7 +4,7 @@ export default axios.create({
 	baseURL:
 		process.env.NODE_ENV === "development"
 			? "http://127.0.0.1:8000"
-			: "http://172.93.48.21:9876"
+			: "https://healthdbbackend.herokuapp.com/"
 });
 
 // export default axios.create({
